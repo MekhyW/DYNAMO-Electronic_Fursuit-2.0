2c84b7c8324c402a7dc1a7ae85378d4ed7599630
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-08-04 00:54:15 -0300

    Create Eyetracking_test.py

---
+++ Test scripts/Eyetracking_test.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+import mediapipe as mp
+displacement_eye = (0,0)
+left_eye_closed = False
+right_eye_closed = False
+mp_face_mesh = mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
+drawSpec = mp_drawing.DrawingSpec(thickness=1, circle_radius=2)
+face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+cap = cv2.VideoCapture(0)
+
+RIGHT_EYE = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398]
+LEFT_EYE = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246] 
+LEFT_IRIS = [468, 469, 470, 471, 472]
+RIGHT_IRIS = [473, 474, 475, 476, 477]
+
+def fit_ellipse(points):
+    ellipse = cv2.fitEllipse(points)
+    center, axes = ellipse[0], ellipse[1]
+    return center, axes
+
+def calculate_eye_displacement(eye_points, iris_points):
+    center, axes = fit_ellipse(iris_points)
+    eye_center = np.mean(eye_points, axis=0)
+    displacement = (2*(center[0] - eye_center[0]) / (axes[0]), 2*(center[1] - eye_center[1]) / (axes[1]))
+    return displacement
+
+def eye_track(frame):
+    global displacement_eye, left_eye_closed, right_eye_closed
+    H, W, _ = frame.shape
+    rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results_mesh = face_mesh.process(rgb_image)
+    if results_mesh.multi_face_landmarks:
+        mesh_points = np.array([np.multiply([p.x, p.y], [W, H]).astype(int) for p in results_mesh.multi_face_landmarks[0].landmark])
+        lex1, ley1 = np.min(mesh_points[LEFT_EYE], axis=0)
+        lex2, ley2 = np.max(mesh_points[LEFT_EYE], axis=0)
+        rex1, rey1 = np.min(mesh_points[RIGHT_EYE], axis=0)
+        rex2, rey2 = np.max(mesh_points[RIGHT_EYE], axis=0)
+        displacement_left_eye = calculate_eye_displacement(mesh_points[LEFT_EYE], mesh_points[LEFT_IRIS])
+        displacement_right_eye = calculate_eye_displacement(mesh_points[RIGHT_EYE], mesh_points[RIGHT_IRIS])
+        displacement_eye = ((displacement_left_eye[0]+displacement_right_eye[0])/2, (displacement_left_eye[1]+displacement_right_eye[1])/2)
+        if abs(lex1-lex2)/abs(ley1-ley2) > 4:
+            left_eye_closed = True
+        else:
+            left_eye_closed = False
+        if abs(rex1-rex2)/abs(rey1-rey2) > 4:
+            right_eye_closed = True
+        else:
+            right_eye_closed = False
+        for faceLms in results_mesh.multi_face_landmarks:
+            mp_drawing.draw_landmarks(frame, faceLms, mp_face_mesh.FACEMESH_CONTOURS,drawSpec,drawSpec)
+        ellipse_center_L, ellipse_axes_L = fit_ellipse(mesh_points[LEFT_IRIS])
+        ellipse_center_R, ellipse_axes_R = fit_ellipse(mesh_points[RIGHT_IRIS])
+        cv2.ellipse(frame, (int(ellipse_center_L[0]), int(ellipse_center_L[1])), (int(ellipse_axes_L[0]/2), int(ellipse_axes_L[1]/2)), 0, 0, 360, (0, 255, 0), 2)
+        cv2.ellipse(frame, (int(ellipse_center_R[0]), int(ellipse_center_R[1])), (int(ellipse_axes_R[0]/2), int(ellipse_axes_R[1]/2)), 0, 0, 360, (0, 255, 0), 2)
+        cv2.rectangle(frame, (lex1, ley1), (lex2, ley2), (0, 255, 0), 2)
+        cv2.rectangle(frame, (rex1, rey1), (rex2, rey2), (0, 255, 0), 2)
+
+while True:
+    ret, frame = cap.read()
+    eye_track(frame)
+    print(displacement_eye, left_eye_closed, right_eye_closed)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break

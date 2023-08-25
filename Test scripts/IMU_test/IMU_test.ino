b51e0d566c4013fb17d1881a93bd6022c851407e
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-08-24 23:53:40 -0300

    IMU_test

---
+++ Test scripts/IMU_test/IMU_test.ino
@@ -0,0 +1,62 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29, &Wire);
+
+void setup(void)
+{
+  Serial.begin(9600);
+  if (!bno.begin())
+  {
+    Serial.print("No BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+}
+
+void loop(void)
+{
+  sensors_event_t orientationData , angVelocityData , linearAccelData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  printEvent(&orientationData);
+  printEvent(&angVelocityData);
+  printEvent(&linearAccelData);
+  Serial.println("--");
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
+
+void printEvent(sensors_event_t* event) {
+  double x = -1000000, y = -1000000 , z = -1000000;
+  if (event->type == SENSOR_TYPE_ORIENTATION) {
+    Serial.print("Orient:");
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+  }
+  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+    Serial.print("Gyro:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
+    Serial.print("Linear:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else {
+    Serial.print("Unk:");
+  }
+  Serial.print("\tx= ");
+  Serial.print(x);
+  Serial.print(" |\ty= ");
+  Serial.print(y);
+  Serial.print(" |\tz= ");
+  Serial.println(z);
+}

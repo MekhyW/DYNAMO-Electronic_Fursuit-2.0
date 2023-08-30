e2db28b9d7c47403134988aa63fe4a1ac7bcc164
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-08-30 13:20:05 -0300

    Update Unity_test.py

--- Test scripts/Unity_test.py
+++ Test scripts/Unity_test.py
@@ -8,12 +8,12 @@ def send_and_receive():
     client_socket.connect((host, port))
     
     while True:
-        displacement_eye_x = input("Enter eye x: ")
-        displacement_eye_y = input("Enter eye y: ")
+        displacement_eye_x = input("Enter eye x: ").replace(".", ",")
+        displacement_eye_y = input("Enter eye y: ").replace(".", ",")
         left_eye_closed = input("Enter left eye closed (True or False): ") == "True"
         right_eye_closed = input("Enter right eye closed (True or False): ") == "True"
-        expression = input("Enter expression: ")
-        message = f"{displacement_eye_x} {displacement_eye_y} {left_eye_closed} {right_eye_closed} {expression}"
+        expression_scores = input("Enter the 7 expression scores (separated by spaces): ").replace(".", ",")
+        message = f"{displacement_eye_x} {displacement_eye_y} {left_eye_closed} {right_eye_closed} {expression_scores}"
         client_socket.sendall(message.encode())
         response = client_socket.recv(1024).decode()
         print("Received response:", response)

1868c637d6cc27a0e2e3f82d9c0ffdd4a0ea8175
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-09-23 11:54:41 -0300

    Update ControlBot.py

--- Main software/ControlBot.py
+++ Main software/ControlBot.py
@@ -1,4 +1,5 @@
 import Waveform
+import Windows
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
@@ -6,12 +7,13 @@ import traceback
 import threading
 import json
 import os
+import time
 
 Token = json.load(open('credentials.json'))['fursuitbot_token']
 ownerID = json.load(open('credentials.json'))['fursuitbot_ownerID']
 fursuitbot = telepot.Bot(Token)
 
-main_menu_buttons = ['Media / Sound', 'Expression', 'Eye Tracking', 'Animatronic', 'LEDs', 'Voice', 'Cookiebot (Assistant AI)', 'Refsheet / Sticker Pack', 'Debugging', 'Shutdown']
+main_menu_buttons = ['🎵 Media / Sound', '😁 Expression', '👀 Eye Tracking', '⚙️ Animatronic', '💡 LEDs', '🎙️ Voice', '🍪 Cookiebot (Assistant AI)', '🖼️ Refsheet / Sticker Pack', '🔧 Debugging', '🛑 Shutdown']
 main_menu_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=button)] for button in main_menu_buttons], resize_keyboard=True)
 inline_keyboard_mediasound = [[{'text': 'Play Music', 'callback_data': 'music'}, {'text': 'Play Sound Effect', 'callback_data': 'sfx'}], [{'text': 'Stop Media', 'callback_data': 'media stop'}, {'text': 'Pause Media', 'callback_data': 'media pause'}, {'text': 'Resume Media', 'callback_data': 'media resume'}], [{'text': 'Set Volume', 'callback_data': 'volume'}]]
 inline_keyboard_expression = [[{'text': 'Change Expression', 'callback_data': 'expression set'}, {'text': 'Set to AUTOMATIC', 'callback_data': 'expression auto'}, {'text': 'Set to MANUAL', 'callback_data': 'expression manual'}]]
@@ -27,6 +29,8 @@ inline_keyboard_shutdown = [[{'text': 'Shutdown', 'callback_data': 'shutdown tur
 last_message_chat = {}
 
 def PlayMusic(fursuitbot, chat_id, text):
+    Waveform.stop_flag = True
+    time.sleep(1)
     for file in os.listdir('.'):
         if file.endswith('.wav'):
             os.remove(file)
@@ -47,6 +51,7 @@ def DiscardPreviousUpdates():
         last_update_id = updates[-1]['update_id']
         fursuitbot.getUpdates(offset=last_update_id+1)
 
+
 def thread_function(msg):
     try:
         content_type, chat_type, chat_id = telepot.glance(msg)
@@ -63,25 +68,25 @@ def thread_function(msg):
         else:
             fursuitbot.sendChatAction(chat_id, 'typing')
             match msg['text']:
-                case 'Media / Sound':
+                case '🎵 Media / Sound':
                     fursuitbot.sendMessage(chat_id, 'Media', reply_markup={'inline_keyboard': inline_keyboard_mediasound})
-                case 'Expression':
+                case '😁 Expression':
                     fursuitbot.sendMessage(chat_id, 'Expression', reply_markup={'inline_keyboard': inline_keyboard_expression})
-                case 'Eye Tracking':
+                case '👀 Eye Tracking':
                     fursuitbot.sendMessage(chat_id, 'Eye Tracking', reply_markup={'inline_keyboard': inline_keyboard_eyetracking})
-                case 'Animatronic':
+                case '⚙️ Animatronic':
                     fursuitbot.sendMessage(chat_id, 'Animatronic', reply_markup={'inline_keyboard': inline_keyboard_animatronic})
-                case 'LEDs':
+                case '💡 LEDs':
                     fursuitbot.sendMessage(chat_id, 'LEDs', reply_markup={'inline_keyboard': inline_keyboard_leds})
-                case 'Voice':
+                case '🎙️ Voice':
                     fursuitbot.sendMessage(chat_id, 'Voice', reply_markup={'inline_keyboard': inline_keyboard_voice})
-                case 'Cookiebot (Assistant AI)':
+                case '🍪 Cookiebot (Assistant AI)':
                     fursuitbot.sendMessage(chat_id, 'Cookiebot', reply_markup={'inline_keyboard': inline_keyboard_cookiebot})
-                case 'Refsheet / Sticker Pack':
+                case '🖼️ Refsheet / Sticker Pack':
                     fursuitbot.sendMessage(chat_id, 'Refsheet / Sticker Pack', reply_markup={'inline_keyboard': inline_keyboard_refsheet})
-                case 'Debugging':
+                case '🔧 Debugging':
                     fursuitbot.sendMessage(chat_id, 'Debugging', reply_markup={'inline_keyboard': inline_keyboard_debugging})
-                case 'Shutdown':
+                case '🛑 Shutdown':
                     fursuitbot.sendMessage(chat_id, 'Shutdown', reply_markup={'inline_keyboard': inline_keyboard_shutdown})
     except Exception as e:
         print(e)
@@ -90,6 +95,7 @@ def thread_function(msg):
             fursuitbot.sendMessage(ownerID, str(msg))
     finally:
         last_message_chat[chat_id] = msg
+
 
 def thread_function_query(msg):
     try:
@@ -112,7 +118,15 @@ def thread_function_query(msg):
                         Waveform.is_paused = False
                 fursuitbot.answerCallbackQuery(query_id, text='Done!')
             case 'volume':
-                pass
+                persist_msg = True
+                if len(query_data.split()) == 1:
+                    fursuitbot.editMessageText((from_id, msg['message']['message_id']), 'Set Volume', reply_markup={'inline_keyboard': [[{'text': '⬅️ Go back', 'callback_data': 'volume goback'}], [{'text': '0%', 'callback_data': 'volume 0'}], [{'text': '25%', 'callback_data': 'volume 25'}], [{'text': '50%', 'callback_data': 'volume 50'}], [{'text': '75%', 'callback_data': 'volume 75'}], [{'text': '100%', 'callback_data': 'volume 100'}]]})
+                elif query_data.split()[1] == 'goback':
+                    fursuitbot.editMessageText((from_id, msg['message']['message_id']), 'Media', reply_markup={'inline_keyboard': inline_keyboard_mediasound})
+                else:
+                    Windows.set_system_volume(int(query_data.split()[1]) / 100)
+                    fursuitbot.editMessageText((from_id, msg['message']['message_id']), 'Volume set to {}%'.format(query_data.split()[1]))
+                    fursuitbot.answerCallbackQuery(query_id, text='Done!')
             case 'expression':
                 match ' '.join(query_data.split()[1:]):
                     case 'set':
@@ -168,21 +182,33 @@ def thread_function_query(msg):
                     case 'stickerpack':
                         pass
             case 'debugging':
-                match ' '.join(query_data.split()[1:]):
-                    case 'resources':
-                        pass
-                    case 'python':
-                        pass
-                    case 'shell':
-                        pass
+                if int(from_id) == int(ownerID):
+                    match ' '.join(query_data.split()[1:]):
+                        case 'resources':
+                            pass
+                        case 'python':
+                            pass
+                        case 'shell':
+                            pass
+                else:
+                    fursuitbot.answerCallbackQuery(query_id, text='This is only available to the user of this fursuit!')
             case 'shutdown':
-                match ' '.join(query_data.split()[1:]):
-                    case 'turnoff':
-                        pass
-                    case 'reboot':
-                        pass
-                    case 'kill':
-                        pass
+                if int(from_id) == int(ownerID):
+                    fursuitbot.deleteMessage((from_id, msg['message']['message_id']))
+                    match ' '.join(query_data.split()[1:]):
+                        case 'turnoff':
+                            fursuitbot.sendMessage(from_id, 'Shutting down...')
+                            Windows.shutdown()
+                        case 'reboot':
+                            fursuitbot.sendMessage(from_id, 'Rebooting...')
+                            Windows.restart()
+                        case 'kill':
+                            fursuitbot.sendMessage(from_id, 'Killing software...')
+                            Windows.kill_process('Unity.exe')
+                            Windows.kill_process('VoicemodDesktop.exe') 
+                            os._exit(0)
+                else:
+                    fursuitbot.answerCallbackQuery(query_id, text='This is only available to the user of this fursuit!')
         if not persist_msg:
             fursuitbot.deleteMessage((from_id, msg['message']['message_id']))
     except Exception as e:
@@ -193,6 +219,7 @@ def thread_function_query(msg):
     finally:
         last_message_chat[from_id] = msg
 
+
 def handle(msg):
     try:
         new_thread = threading.Thread(target=thread_function, args=(msg,))
@@ -200,12 +227,14 @@ def handle(msg):
     except:
         fursuitbot.sendMessage(ownerID, traceback.format_exc())
 
+
 def handle_query(msg):
     try:
         new_thread = threading.Thread(target=thread_function_query, args=(msg,))
         new_thread.start()
     except:
         fursuitbot.sendMessage(ownerID, traceback.format_exc())
+
 
 def StartBot():
     global bot_online

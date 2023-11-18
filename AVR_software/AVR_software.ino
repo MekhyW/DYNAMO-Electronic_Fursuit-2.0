e89b0d0dac2a0fcf83676e7629ed4881db329f20
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-11-18 00:12:53 -0300

    Optimizations AVR_software.ino

--- AVR_software/AVR_software.ino
+++ AVR_software/AVR_software.ino
@@ -5,6 +5,9 @@
 #include "Servos.h"
 #include "Serial.h"
 #include "IMU.h"
+#define TASK_STACK_SIZE 128
+#define QUEUE_SIZE 10
+#define TASK_DELAY_MS 10
 
 QueueHandle_t queue_leds;
 QueueHandle_t queue_servos;
@@ -15,11 +18,15 @@ void setup() {
   Serial.println("Starting up...");
   setupLEDs();
   setupServos();
-  xTaskCreate(TaskReadSerial, "ReadSerial", 128, NULL, 1, NULL);
-  xTaskCreate(TaskLEDs, "LEDs", 128, NULL, 1, NULL);
-  xTaskCreate(TaskServos, "Servos", 128, NULL, 1, NULL);
-  queue_leds = xQueueCreate(10, sizeof(LEDsTaskInput));
-  queue_servos = xQueueCreate(10, sizeof(ServosTaskInput));
+  xTaskCreate(TaskReadSerial, "ReadSerial", TASK_STACK_SIZE, NULL, 1, NULL);
+  xTaskCreate(TaskLEDs, "LEDs", TASK_STACK_SIZE, NULL, 1, NULL);
+  xTaskCreate(TaskServos, "Servos", TASK_STACK_SIZE, NULL, 1, NULL);
+  queue_leds = xQueueCreate(QUEUE_SIZE, sizeof(LEDsTaskInput));
+  queue_servos = xQueueCreate(QUEUE_SIZE, sizeof(ServosTaskInput));
+  if (queue_leds == NULL || queue_servos == NULL) {
+    Serial.println("Error: failed to create queue");
+    while (1) {;}
+  }
 }
 
 void loop() {}
@@ -30,10 +37,10 @@ void loop() {}
 
 void TaskReadSerial(void *pvParameters) {
   (void) pvParameters;
+  LEDsTaskInput leds_input;
+  ServosTaskInput servos_input;
   for (;;) {
     updateSerial();
-    LEDsTaskInput leds_input;
-    ServosTaskInput servos_input;
     servos_input.animatronics_on = inputs[0];
     leds_input.leds_on = inputs[1];
     leds_input.leds_brightness = inputs[2];
@@ -46,16 +53,22 @@ void TaskReadSerial(void *pvParameters) {
     servos_input.emotion_neutral = inputs[9];
     servos_input.emotion_sad = inputs[10];
     servos_input.emotion_surprised = inputs[11];
-    xQueueSendToFront(queue_leds, &leds_input, 0);
-    xQueueSendToFront(queue_servos, &servos_input, 0);
-    vTaskDelay(10);
+    if (xQueueSendToBack(queue_leds, &leds_input, 0) != pdTRUE) {
+      xQueueReceive(queue_leds, &leds_input, 0);
+      xQueueSendToBack(queue_leds, &leds_input, 0);
+    }
+    if (xQueueSendToBack(queue_servos, &servos_input, 0) != pdTRUE) {
+      xQueueReceive(queue_servos, &servos_input, 0);
+      xQueueSendToBack(queue_servos, &servos_input, 0);
+    }
+    vTaskDelay(TASK_DELAY_MS / portTICK_PERIOD_MS);
   }
 }
 
 void TaskLEDs(void *pvParameters) {
   (void) pvParameters;
+  LEDsTaskInput leds_input;
   for (;;) {
-    LEDsTaskInput leds_input;
     xQueueReceive(queue_leds, &leds_input, portMAX_DELAY);
     if (leds_input.leds_on == 1) {
       Color_Brightness = leds_input.leds_brightness;
@@ -78,14 +91,14 @@ void TaskLEDs(void *pvParameters) {
     } else {
       off();
     }
-    vTaskDelay(10);
+    vTaskDelay(TASK_DELAY_MS / portTICK_PERIOD_MS);
   }
 }
 
 void TaskServos(void *pvParameters) {
   (void) pvParameters;
+  ServosTaskInput servos_input;
   for (;;) {
-    ServosTaskInput servos_input;
     xQueueReceive(queue_servos, &servos_input, portMAX_DELAY);
     int expressions_sum = servos_input.emotion_angry + servos_input.emotion_disgusted + servos_input.emotion_happy
                             + servos_input.emotion_neutral + servos_input.emotion_sad + servos_input.emotion_surprised;
@@ -101,6 +114,6 @@ void TaskServos(void *pvParameters) {
       }
       writepos(pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], pos[6], pos[7], pos[8]);
     }
-    vTaskDelay(10);
+    vTaskDelay(TASK_DELAY_MS / portTICK_PERIOD_MS);
   }
 }

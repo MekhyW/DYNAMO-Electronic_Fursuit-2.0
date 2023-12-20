90dc53e342d11e1b191c60495d127e2e6a593e4c
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-12-20 19:58:54 -0300

    Update Launch.bat

--- Launch.bat
+++ Launch.bat
@@ -2,19 +2,22 @@
 
 :: Update the "Eye-Graphics" repository
 cd "C:\Users\LattePanda\Documents\GitHub\Eye-Graphics"
-git pull
+git pull || echo Git pull failed
 cd "..\"
 
 :: Update the "Eye-Graphics" repository
 cd "DYNAMO-Electronic_Fursuit-2.0"
-git pull
+git pull || echo Git pull failed
 
 :: Start Voicemod
-start "" "C:\Program Files\Voicemod Desktop\VoicemodDesktop.exe"
+start "" "C:\Program Files\Voicemod Desktop\VoicemodDesktop.exe" || echo Voicemod failed to start
 
 :: Start Eye-Graphics.exe
-start "" "C:\Users\LattePanda\Documents\GitHub\Eye-Graphics\Eye-Graphics.exe"
+start "" "C:\Users\LattePanda\Documents\GitHub\Eye-Graphics\Eye-Graphics.exe" || echo Eye-Graphics failed to start
 
 :: Start DYNAMO.py
 cd "Main software"
-python DYNAMO.py
+python DYNAMO.py || echo DYNAMO.py failed to start
+
+:: Pause at the end
+pause

22acd5880475e3aec1e4761e2959ba5f9bb67c52
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-10-26 02:48:41 -0300

    Update Unity.py

--- Main software/Unity.py
+++ Main software/Unity.py
@@ -24,11 +24,11 @@ def connect():
     return None
 
 def send(displacement_eye_x, displacement_eye_y, closeness_left, closeness_right, emotion_scores, manual_mode, manual_id, silly_mode):
-    displacement_eye_x = str(displacement_eye_x).replace(".", ",")
-    displacement_eye_y = str(displacement_eye_y).replace(".", ",")
-    closeness_left = str(closeness_left).replace(".", ",")
-    closeness_right = str(closeness_right).replace(".", ",")
-    emotion_scores = [str(0) if score < 0.01 else str(score).replace(".", ",") for score in emotion_scores]
+    displacement_eye_x = str(displacement_eye_x).replace(",", ".")
+    displacement_eye_y = str(displacement_eye_y).replace(",", ".")
+    closeness_left = str(closeness_left).replace(",", ".")
+    closeness_right = str(closeness_right).replace(",", ".")
+    emotion_scores = [str(0) if score < 0.01 else str(score).replace(",", ".") for score in emotion_scores]
     emotion_scores_string = " ".join(emotion_scores)
     message = f"{displacement_eye_x} {displacement_eye_y} {closeness_left} {closeness_right} {emotion_scores_string}"
     message += f" {manual_id}" if manual_mode and manual_id >= len(emotion_scores) else " -1"

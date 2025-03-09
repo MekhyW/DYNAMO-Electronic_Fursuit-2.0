de2ce561f35d8d1554670a4d1c1fbca8050b996e
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-03-09 17:13:15 -0300

    face_landmarker_v2_with_blendshapes

--- Main software/MachineVision.py
+++ Main software/MachineVision.py
@@ -1,32 +1,33 @@
 import numpy as np
 from scipy.special import expit
-import math
 import cv2
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
 import pickle
 import joblib
 import warnings
 warnings.filterwarnings("ignore")
 
-mp_face_mesh = mp.solutions.face_mesh
-mp_drawing = mp.solutions.drawing_utils
-drawSpec = mp_drawing.DrawingSpec(thickness=1, circle_radius=2)
-face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+detector = vision.FaceLandmarker.create_from_options(
+    vision.FaceLandmarkerOptions(base_options=python.BaseOptions(model_asset_path='models/face_landmarker_v2_with_blendshapes.task'),
+    output_face_blendshapes=True,
+    num_faces=1
+))
 emotion_model = pickle.load(open('models/emotion_model.pkl', 'rb'))
 pca_model = joblib.load('models/pca_model.pkl')
-eye_closeness_model = pickle.load(open('models/eyecloseness_model.pkl', 'rb'))
-RIGHT_EYE = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398, 362]
-LEFT_EYE = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246, 33] 
 LEFT_IRIS = [468, 469, 470, 471, 472]
 RIGHT_IRIS = [473, 474, 475, 476, 477]
 EMOTION_LABELS = ['angry', 'disgusted', 'happy', 'neutral', 'sad', 'surprised']
 EMOTION_LABELS_EXTRA = ['hypnotic', 'heart', 'rainbow', 'nightmare', 'gears', 'sans', 'mischievous']
-CROSS_EYEDNESS_THRESHOLD = 0.5
+CROSS_EYEDNESS_THRESHOLD = 0.4
 UPSIDE_DOWN = True
 
 cap = None
 cap_id = 0
-results_mesh = None
+detection_result = None
 mesh_points = None
 displacement_eye = (0,0)
 left_eye_closeness = 0
@@ -83,67 +84,45 @@ def transform_to_zero_one_numpy(arr, normalize=False):
         transformed_arr /= np.sum(transformed_arr)
     return transformed_arr
 
-def calculate_proximity(pointA, pointB, pointMid):
-    dist_MA = math.sqrt((pointA[0] - pointMid[0])**2 + (pointA[1] - pointMid[1])**2 + (pointA[2] - pointMid[2])**2)
-    dist_MB = math.sqrt((pointB[0] - pointMid[0])**2 + (pointB[1] - pointMid[1])**2 + (pointB[2] - pointMid[2])**2)
-    return 2 * (dist_MA / (dist_MA + dist_MB)) - 1
-
 def calculate_eye_displacement(iris_points, ex1, ex2, ey1, ey2):
     eye_center = np.mean(iris_points, axis=0)
-    displacement_x = 2*calculate_proximity(ex1, ex2, eye_center)
-    displacement_y = -2*calculate_proximity(ey1, ey2, eye_center)
-    displacement = (displacement_x, displacement_y)
-    return displacement
+    ex1, ex2 = np.array(ex1), np.array(ex2)
+    ey1, ey2 = np.array(ey1), np.array(ey2)
+    dist_x1 = np.linalg.norm(ex1 - eye_center)
+    dist_x2 = np.linalg.norm(ex2 - eye_center)
+    dist_y1 = np.linalg.norm(ey1 - eye_center)
+    dist_y2 = np.linalg.norm(ey2 - eye_center)
+    displacement_x = 2 * (dist_x1 / (dist_x1 + dist_x2)) - 1
+    displacement_y = -2 * (dist_y1 / (dist_y1 + dist_y2)) - 1
+    return (displacement_x, displacement_y)
 
-def calculate_sclera_area(mesh_points):
-    lex1, lex2 = mesh_points[33], mesh_points[133]
-    rex1, rex2 = mesh_points[362], mesh_points[263]
-    area_sclera_left = 0
-    area_sclera_right = 0
-    for i in range(len(LEFT_EYE) - 1):
-        area_sclera_left += (mesh_points[LEFT_EYE[i], 0] * mesh_points[LEFT_EYE[i + 1], 1]) - (mesh_points[LEFT_EYE[i + 1], 0] * mesh_points[LEFT_EYE[i], 1])
-    for i in range(len(RIGHT_EYE) - 1):
-        area_sclera_right += (mesh_points[RIGHT_EYE[i], 0] * mesh_points[RIGHT_EYE[i + 1], 1]) - (mesh_points[RIGHT_EYE[i + 1], 0] * mesh_points[RIGHT_EYE[i], 1])
-    area_sclera_left = abs(area_sclera_left / (2*(math.sqrt((lex1[0]-lex2[0])**2 + (lex1[1]-lex2[1])**2)**2)))
-    area_sclera_right = abs(area_sclera_right / (2*(math.sqrt((rex1[0]-rex2[0])**2 + (rex1[1]-rex2[1])**2)**2)))
-    return area_sclera_left, area_sclera_right
+def inference(cv2image):
+    global mesh_points, detection_result
+    if cv2image is None:
+        return None
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(cv2image, cv2.COLOR_BGR2RGB))
+    detection_result = detector.detect(image)
+    if detection_result is None or not len(detection_result.face_blendshapes):
+        return None
+    blendshapes = detection_result.face_blendshapes[0]
+    landmarks = detection_result.face_landmarks[0]
+    blendshapes_scores = [cat.score for cat in blendshapes]
+    mesh_points = np.array([[p.x, p.y, p.z] for p in landmarks])
+    normalized_mesh_points = np.linalg.norm(mesh_points - mesh_points[151], axis=1) / np.linalg.norm(mesh_points[4] - mesh_points[151])
+    return [*blendshapes_scores, *normalized_mesh_points]
 
-def calculate_width_over_height_eye(mesh_points):
-    lex1 = mesh_points[33]
-    lex2 = mesh_points[133]
-    rex1 = mesh_points[362]
-    rex2 = mesh_points[263]
-    ley1 = mesh_points[159]
-    ley2 = mesh_points[145]
-    rey1 = mesh_points[386]
-    rey2 = mesh_points[374]
-    try:
-        reason_left = math.sqrt((lex1[0]-lex2[0])**2+(lex1[1]-lex2[1])**2+(lex1[2]-lex2[2])**2)/math.sqrt((ley1[0]-ley2[0])**2+(ley1[1]-ley2[1])**2+(ley1[2]-ley2[2])**2)
-        if reason_left > 20: reason_left = 20
-    except ZeroDivisionError:
-        reason_left = 20
-    try:
-        reason_right = math.sqrt((rex1[0]-rex2[0])**2+(rex1[1]-rex2[1])**2+(rex1[2]-rex2[2])**2)/math.sqrt((rey1[0]-rey2[0])**2+(rey1[1]-rey2[1])**2+(rey1[2]-rey2[2])**2)
-        if reason_right > 20: reason_right = 20
-    except ZeroDivisionError:
-        reason_right = 20
-    return reason_left, reason_right
-
-def update_mesh_points(frame):
-    global results_mesh, mesh_points
-    H, W, _ = frame.shape
-    results_mesh = face_mesh.process(frame)
-    if results_mesh.multi_face_landmarks:
-        mesh_points = np.array([np.multiply([p.x, p.y, p.z], [W, H, max(W, H)]).astype(int) for p in results_mesh.multi_face_landmarks[0].landmark])
-
-def draw_tracking(frame):
-    global results_mesh, mesh_points
-    if results_mesh.multi_face_landmarks is not None:
-        for faceLms in results_mesh.multi_face_landmarks:
-            mp_drawing.draw_landmarks(frame, faceLms, mp_face_mesh.FACEMESH_CONTOURS,drawSpec,drawSpec)
-        cv2.circle(frame, (int(np.mean(mesh_points[LEFT_IRIS], axis=0)[0]), int(np.mean(mesh_points[LEFT_IRIS], axis=0)[1])), 5, (0, 0, 255), -1)
-        cv2.circle(frame, (int(np.mean(mesh_points[RIGHT_IRIS], axis=0)[0]), int(np.mean(mesh_points[RIGHT_IRIS], axis=0)[1])), 5, (0, 0, 255), -1)
-    return frame
+def draw_tracking(rgb_image):
+  global detection_result
+  face_landmarks_list = detection_result.face_landmarks
+  annotated_image = np.copy(rgb_image)
+  for idx in range(len(face_landmarks_list)):
+    face_landmarks = face_landmarks_list[idx]
+    face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    face_landmarks_proto.landmark.extend([landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks])
+    solutions.drawing_utils.draw_landmarks(image=annotated_image,landmark_list=face_landmarks_proto,connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,landmark_drawing_spec=None,connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_tesselation_style())
+    solutions.drawing_utils.draw_landmarks(image=annotated_image,landmark_list=face_landmarks_proto,connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,landmark_drawing_spec=None,connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_contours_style())
+    solutions.drawing_utils.draw_landmarks(image=annotated_image,landmark_list=face_landmarks_proto,connections=mp.solutions.face_mesh.FACEMESH_IRISES,landmark_drawing_spec=None,connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_iris_connections_style())
+  return annotated_image
 
 def draw_emotion(frame, emotion):
     if emotion:
@@ -160,20 +139,12 @@ def draw_gaze(frame, displacement_eye, cross_eyedness):
     cv2.putText(frame, f"Crosseyed: {round(cross_eyedness, 2)}", (10, 180), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
     return frame
 
-def predict_emotion(frame, draw=False):
+def predict_emotion(frame, scores, draw=False):
     global mesh_points, EMOTION_LABELS, emotion_scores
     if not expression_manual_mode:
-        if mesh_points is None:
+        if scores is None:
             return None
-        nose_tip = mesh_points[4] 
-        forehead = mesh_points[151]
-        mesh_norm = mesh_points - nose_tip
-        scale_factor = np.linalg.norm(forehead - nose_tip)
-        if np.isclose(scale_factor, 0):
-            scale_factor = 1e-6
-        mesh_norm = np.divide(mesh_norm, scale_factor)
-        landmarks_flat = mesh_norm.flatten()
-        landmarks_transformed = pca_model.transform([landmarks_flat])
+        landmarks_transformed = pca_model.transform([scores])
         pred = emotion_model.predict_proba(landmarks_transformed)[0]
         emotion_scores_noisy = transform_to_zero_one_numpy(pred)
         for score in range(len(emotion_scores)):
@@ -190,33 +161,22 @@ def predict_emotion(frame, draw=False):
             frame = draw_emotion(frame, EMOTION_LABELS[expression_manual_id])
     return frame
 
-def calculate_eye_closeness(mesh_points):
-    global left_eye_closeness, right_eye_closeness, left_eye_closeness_noisy, right_eye_closeness_noisy
-    area_sclera_left, area_sclera_right = calculate_sclera_area(mesh_points)
-    reason_left, reason_right = calculate_width_over_height_eye(mesh_points)
-    pred_left = transform_to_zero_one_numpy(eye_closeness_model.predict_proba([[area_sclera_left, reason_left]])[0], normalize=True)
-    pred_right = transform_to_zero_one_numpy(eye_closeness_model.predict_proba([[area_sclera_right, reason_right]])[0], normalize=True)
-    left_eye_closeness, left_eye_closeness_noisy = rolling_average(left_eye_closeness_noisy, 1.2 - (pred_left[0]*1.2 + pred_left[1]*1 + pred_left[2]*0.5))
-    right_eye_closeness, right_eye_closeness_noisy = rolling_average(right_eye_closeness_noisy, 1.2 - (pred_right[0]*1.2 + pred_right[1]*1 + pred_right[2]*0.5))
-    return left_eye_closeness, right_eye_closeness
-
-def eye_track(frame, draw=False):
-    global results_mesh, mesh_points, displacement_eye, left_eye_closeness, right_eye_closeness, cross_eyedness, cross_eyedness_noisy
-    if eye_tracking_mode:
-        H, W, _ = frame.shape
-        if mesh_points is not None:
-            left_eye_closeness, right_eye_closeness = calculate_eye_closeness(mesh_points)
-            displacement_left_eye = calculate_eye_displacement(mesh_points[LEFT_IRIS], mesh_points[33], mesh_points[133], mesh_points[159], mesh_points[145])
-            displacement_right_eye = calculate_eye_displacement(mesh_points[RIGHT_IRIS], mesh_points[362], mesh_points[263], mesh_points[386], mesh_points[374])
-            displacement_left_eye = (max(min(1, displacement_left_eye[0]), -1), max(min(1, displacement_left_eye[1]), -1))
-            displacement_right_eye = (max(min(1, displacement_right_eye[0]), -1), max(min(1, displacement_right_eye[1]), -1))
-            displacement_eye_noisy = ((displacement_left_eye[0]+displacement_right_eye[0])/2, (displacement_left_eye[1]+displacement_right_eye[1])/2)
-            displacement_eye = 0.5*np.array(displacement_eye) + 0.5*np.array(displacement_eye_noisy)
-            cross_eyedness, cross_eyedness_noisy = rolling_average(cross_eyedness_noisy, displacement_left_eye[0] - displacement_right_eye[0])
-            if draw:
-                frame = draw_tracking(frame)
-                frame = draw_eyecloseness(frame, left_eye_closeness, right_eye_closeness)
-                frame = draw_gaze(frame, displacement_eye, cross_eyedness)
+def eye_track(frame, scores, draw=False):
+    global detection_result, mesh_points, displacement_eye, left_eye_closeness, right_eye_closeness, left_eye_closeness_noisy, right_eye_closeness_noisy, cross_eyedness, cross_eyedness_noisy
+    if eye_tracking_mode and scores:
+        left_eye_closeness, left_eye_closeness_noisy = rolling_average(left_eye_closeness_noisy, scores[9]*1.2  + scores[19]*0.6 - scores[21]*1.2)
+        right_eye_closeness, right_eye_closeness_noisy = rolling_average(right_eye_closeness_noisy, scores[10]*1.2 + scores[20]*0.6 - scores[22]*1.2)
+        displacement_left_eye = calculate_eye_displacement(mesh_points[LEFT_IRIS], mesh_points[33], mesh_points[133], mesh_points[159], mesh_points[145])
+        displacement_right_eye = calculate_eye_displacement(mesh_points[RIGHT_IRIS], mesh_points[362], mesh_points[263], mesh_points[386], mesh_points[374])
+        displacement_left_eye = (max(min(1, displacement_left_eye[0]), -1), max(min(1, displacement_left_eye[1]), -1))
+        displacement_right_eye = (max(min(1, displacement_right_eye[0]), -1), max(min(1, displacement_right_eye[1]), -1))
+        displacement_eye_noisy = ((displacement_left_eye[0]+displacement_right_eye[0])/2, (displacement_left_eye[1]+displacement_right_eye[1])/2)
+        displacement_eye = 0.5*np.array(displacement_eye) + 0.5*np.array(displacement_eye_noisy)
+        cross_eyedness, cross_eyedness_noisy = rolling_average(cross_eyedness_noisy, displacement_left_eye[0] - displacement_right_eye[0])
+        if draw:
+            frame = draw_tracking(frame)
+            frame = draw_eyecloseness(frame, left_eye_closeness, right_eye_closeness)
+            frame = draw_gaze(frame, displacement_eye, cross_eyedness)
     else:
         displacement_eye = (0,0)
         left_eye_closeness = 0.2
@@ -225,12 +185,12 @@ def eye_track(frame, draw=False):
     return frame
 
 def main(draw=False):
-    ret, frame = cap.read()
+    _, frame = cap.read()
     if frame is not None:
         frame = undistort_image(frame)
-        update_mesh_points(frame)
-        frame = eye_track(frame, draw=draw)
-        frame = predict_emotion(frame, draw=draw)
+        scores = inference(frame)
+        frame = eye_track(frame, scores, draw=draw)
+        frame = predict_emotion(frame, scores, draw=draw)
         if draw:
             try:
                 cv2.imshow('frame', frame)

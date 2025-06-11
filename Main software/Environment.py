15525d3d7b8309877e141d9d9f900236dc282947
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-06-11 01:02:35 -0300

    Update Environment.py

--- Main software/Environment.py
+++ Main software/Environment.py
@@ -8,10 +8,18 @@ try:
     fursuitbot_ownerID = os.getenv("fursuitbot_ownerID")
     openai_key = os.getenv("openai_key")
     porcupine_key = os.getenv("porcupine_key")
+    mqtt_host = os.getenv("mqtt_host")
+    mqtt_port = int(os.getenv("mqtt_port"))
+    mqtt_username = os.getenv("mqtt_username")
+    mqtt_password = os.getenv("mqtt_password")
 except Exception as e:
     voicemod_key = None
     fursuitbot_token = None
     fursuitbot_ownerID = None
     openai_key = None
     porcupine_key = None
+    mqtt_host = None
+    mqtt_port = None
+    mqtt_username = None
+    mqtt_password = None
     print(f"Environment variable loading failed with error: {e}")

dcc6ff68eb4f632c20c12f4c6cf7a6df0e3f6bd3
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-07-16 18:16:42 -0300

    Update Serial.py

--- Main software/Serial.py
+++ Main software/Serial.py
@@ -18,7 +18,7 @@ leds_level = 0
 
 def leds_level_from_int16(int16_value):
     global leds_effect, leds_level
-    leds_effect = len(leds_effects_options)  # "Level" effect is last index + 1
+    #leds_effect = len(leds_effects_options)  # "Level" effect is last index + 1
     leds_level = int((int16_value / 32767) * 100) # Convert int16 to percentage int
     return leds_level
 
